eb8d8ec1d39166c551664b9171732c6227b5c689
Bhagyashree-Tondkal <Bhagyatondkal@gmail.com>
2026-09-07 14:47:08 +0530

    first push

---
+++ src/test/java/Contact/Contact.java
@@ -0,0 +1,16 @@
+package Contact;
+
+import org.testng.annotations.Test;
+
+public class Contact{
+	@Test 
+	public void contact() {
+		System.out.println("crate contact");
+		
+	}
+	@Test 
+	public void contactwith() {
+		System.out.println("crate contact with email");
+		
+	}
+}
---
+++ src/test/java/ORG/org.java
@@ -0,0 +1,17 @@
+package ORG;
+
+import org.testng.annotations.Test;
+
+public class org{
+	@Test
+	
+	public void CreateOrgTest() {
+		System.out.println("org");
+	}
+	
+    @Test
+	public void CreateConTest() {
+		System.out.println("contact");
+	}
+	
+}
